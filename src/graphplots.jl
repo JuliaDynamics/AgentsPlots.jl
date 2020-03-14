c956d69ce4f3909503ebb4be5e3c5acdcc30a7f4
Datseris <datseris.george@gmail.com>
2020-03-14 14:43:00 +0100

    modify to return specific color

--- src/graphplots.jl
+++ src/graphplots.jl
@@ -3,31 +3,26 @@ using GraphRecipes, Plots, LightGraphs
 export plotabm
 
 """
-    plotabm(model::ABM, c [, s]; kwargs...)
-Plot the `model` as a graph by providing two **functions** `c, s`.
+    plotabm(model::ABM [, c , s]; kwargs...)
+Plot the `model` as a graph by providing two optional **functions** `c, s`.
 Both of these functions gain as an input the list of agents `a` at a node of
-the model's graph and output a number. `c` returns a number (anything acceptable by
+the model's graph and output a number. `c` returns a *color* (anything acceptable by
 Plots.jl) of the node while `s` returhs the (relative) size of the node.
 
-`s` defaults to `length`. Internally the `graphplot` recipe is used, and thus
+`c` defaults to a purple color for all nodes, while `s` defaults to `length`.
+Internally the `graphplot` recipe is used, and thus
 all keyword arguments are propagated into this recipe.
 """
 function plotabm end
 
-function plotabm(model::ABM, c, s = length; kwargs...)
+function plotabm(model::ABM, c = x -> "#765db4", s = length; kwargs...)
     N = nodes(model)
-    ncolor = zeros(length(N)); weights = copy(ncolor)
+    ncolor = []; weights = zeros(length(N))
     for (i, n) in enumerate(N)
         a = get_node_contents(n, model)
-        ncolor[i] = c(a); weights[i] = s(a)
+        push!(ncolor, c(a)); weights[i] = s(a)
     end
 
-    # Hack until graphplot recipe can actually plot with number and color
-    # https://github.com/JuliaPlots/GraphRecipes.jl/issues/108
-    mi, ma = extrema(ncolor)
-    cgrad(:plasma)
-    colors = [cgrad(:plasma)[(co-mi)/(ma-mi)] for co in ncolor]
-
-    graphplot(model.space.graph, node_weights = weights, nodeshape = :circle,
-              color = "black", markerstrokecolor = "black", markerstrokewidth=1.5, markercolor = colors; kwargs...)
+    graphplot(model.space.graph, node_weights = weights, nodeshape = :circle, nodecolor = ncolor,
+              color = "black", markerstrokecolor = "black", markerstrokewidth=1.5; kwargs...)
 end
