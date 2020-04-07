572549c70a696f28378e58f78ae702406834de23
George Datseris <datseris.george@gmail.com>
2020-04-07 16:52:56 +0200

    rework plotabm, remove plot2D, unify interface (#10)
    * rework plotabm, remove plot2D, unify interface
    
    * increase version
    
    * fix kwargs.

--- src/continuous.jl
+++ src/continuous.jl
@@ -1,17 +1,9 @@
 """
     plotabm(model::ABM{A, <: ContinuousSpace}; ac, as, am, kwargs...)
-Plot all agents of the `model` on continuous space by (optionally)
-configuring how to plot the agents with keywords `ac, as, am`.
+Identical with the discrete space version but now the functions `ac, as, am` take
+a single agent as an input (and have same output as before).
 
-`ac` = agent color, `as` = agent size, `am` = agent marker shape.
-
-These arguments can be constants (numbers, strings, etc.), and then all agents
-get the same property. Or, they can be **functions**, that take as input the
-list of agents, and output a vector of colors, sizes, or shapes
-(anything acceptable by `scatter`).
-
-Defaults: `ac = "#765db4", as = 8, am = :o`. All other keywords are
-propatted into `Plots.scatter`.
+All other keywords are propatted into `Plots.scatter`.
 """
 function plotabm(model::ABM{A, <: ContinuousSpace};
     ac = "#765db4", as = 8, am = :o,
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,3 @@
 using Test
 using AgentsPlots
 using Agents
-
-include("agent_distribution.jl")
-include("CA1D.jl")
-include("CA2D.jl")
