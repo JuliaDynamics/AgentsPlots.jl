0143be819b9c3bf2cb9a383e3ac377dd55866e75
Tim DuBois <tim@neophilus.net>
2020-04-27 22:27:51 +0200

    Align grid implementation closer to continuous space

--- src/grid.jl
+++ src/grid.jl
@@ -1,23 +1,26 @@
 using Plots
 
-function plotabm(model::ABM{A, <: GridSpace};
-        ac = x -> "#765db4", as = x -> 10, am = x -> :circle,
-        kwargs...) where {A}
+function plotabm(
+    model::ABM{A,<:GridSpace};
+    ac = "#765db4",
+    as = 10,
+    am = :circle,
+    kwargs...,
+) where {A}
 
-    D = length(model.space.dimensions)
-    N = nodes(model)
-    ncolor, weights, markers, pos = [], Float64[], Symbol[], NTuple{D, Int}[]
-    for n in N
-        a = get_node_agents(n, model)
-        isempty(a) && continue
-        for (c, f) in zip((ncolor, weights, markers), (ac, as, am))
-            push!(c, f(a))
-        end
-        push!(pos, Agents.vertex2coord(n, model))
-    end
+    aa = allagents(model)
+    colors = typeof(ac) <: Union{String,Tuple,Real} ? ac : [ac(a) for a in aa]
+    sizes = typeof(as) <: Real ? as : [as(a) for a in aa]
+    mshapes = typeof(am) <: Union{Symbol,String} ? am : [am(a) for a in aa]
+    pos = [a.pos for a in aa]
 
     scatter(
-        pos; color = ncolor, ms = weights, marker = markers, label = "",
-        kwargs...
+        pos;
+        markercolor = colors,
+        markersize = sizes,
+        markershapes = mshapes,
+        label = "",
+        kwargs...,
     )
 end
+
